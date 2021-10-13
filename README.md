53d18b496f23696f5a835052d01ea509592597b3
Vincent Plourde <vincent.plourde.1@ulaval.ca>
2021-10-13 16:06:40 -0400

    Ajout début d'idée.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # IFT-2003-A21-EQ22
 Travail pratique 1 IFT2003-Equipe 22
+
+Auteurs:
+Stéphane Grondin
+Vincent Plourde
+Léa Vigneault
---
+++ tp1.pl
@@ -0,0 +1,26 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Fais
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+jeton(rouge).
+jeton(jaune).
+
+%États de départ et état final
+etat_initial([[],[],[],[],[],[],[]]).
+etat_final(). %???
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Prédicats
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Place un jeton dans la colonne s'il y a une place disponible.
+placer_jeton(Colonne, CouleurJeton, [ CouleurJeton | Colonne]):- valider_place_disponible(Colonne).
+
+%Valider s'il reste de la place dans une colonne pour placer une pièce.
+valider_place_disponible(Colonne):- length(Colonne, N), N < 7.
+
+
+valider_verticale().
+valider_horizontale().
+valider_diagonale().
+
+min().
+max().
